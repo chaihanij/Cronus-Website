d742c95c227a629dc7fa5fcaa3815f92e1019eb8
Wachirawat Chaihanij <chaihanij@gmail.com>
2015-07-17 10:57:20 +0700

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   app/models/operating_system.rb
    	modified:   app/views/operating_systems/_form.html.erb

--- app/models/operating_system.rb
+++ app/models/operating_system.rb
@@ -1,4 +1,10 @@
 class OperatingSystem < ActiveRecord::Base
+	
 	has_many :package
+
+	validates :title, length: { maximum: 50, too_long: "%{count} characters is the maximum allowed" }
+   	validates :version, length: { maximum: 20, too_long: "%{count} characters is the maximum allowed" }
+    validates :description, length: { maximum: 255, too_long: "%{count} characters is the maximum allowed" }
+
 	scope :is_public, -> { where(:is_public => 1) }
 end
